e3ea2bdf50c48e4e85b9ac0f147602b873a79283
Saumya Sarkar <21f3001551@ds.study.iitm.ac.in>
2025-07-27 15:21:54 +0530

    celery setup

--- backend/config_file.py
+++ backend/config_file.py
@@ -23,3 +23,10 @@ class localDev(Config):
 
 class Deployment(Config):
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///site.sqlite3")
+
+
+class celeryConfig():
+    broker_url = "redis://localhost:6379/0"
+    result_backend = "redis://localhost:6379/1"
+    timezone = "Asia/Kolkata"
+    broker_connection_retry_on_startup = True
